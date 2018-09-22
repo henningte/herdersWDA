c10377b9673a4284e65e89fe1c503e40833525cd
Henning Teickner <34153309+henningte@users.noreply.github.com>
2018-09-22 10:13:16 +0200

    Added package import (raster, rgdal)

--- R/spatAggregate.R
+++ R/spatAggregate.R
@@ -1,4 +1,6 @@
 #'@importFrom Rdpack reprompt
+#'@import rgdal
+#'@import raster
 NULL
 
 #' Spatially aggregates raster time series within geometries.
@@ -6,19 +8,19 @@ NULL
 #' \code{spatAggregate} performs aggregation of raster based time series
 #' (\code{RasterBrick} or \code{RasterStack} object, see:
 #' \code{\link[raster]{Raster-class}}) within certain spatial entity (polygon geometry)
-#'  applying \code{over} from package \code{sp} on all raster layers (time instances) 
+#'  applying \code{over} from package \code{sp} on all raster layers (time instances)
 #'  within a given time interval.
-#' @param variable A \code{RasterBrick} or \code{RasterStack} object. 
-#' @param spatialentity A \code{SpatialPolygonsDataFrame} of the  spatial entity to 
+#' @param variable A \code{RasterBrick} or \code{RasterStack} object.
+#' @param spatialentity A \code{SpatialPolygonsDataFrame} of the  spatial entity to
 #' iaggregate within..
-#' @param tstart Start of the time interval during which to perform aggregation 
+#' @param tstart Start of the time interval during which to perform aggregation
 #' given as character values in the format "yyyy-mm-dd".
-#' @param tend End of the time interval during which to perform aggregation given 
+#' @param tend End of the time interval during which to perform aggregation given
 #' as character values in the format "yyyy-mm-dd".
-#' @param fn Function to be used for aggregation. One of \code{mean}, \code{min} 
+#' @param fn Function to be used for aggregation. One of \code{mean}, \code{min}
 #' or \code{max}.
 #' @return A data frame containing the aggregated vaules of each raster layer (time instance)
-#' within each spatial entity 
+#' within each spatial entity
 #' @seealso
 #' @examples #
 #' @export
@@ -27,7 +29,7 @@ spatAggregate <- function(variable, spatialentity, tstart, tend, fn = "mean"){
   tstart = as.POSIXct(substr(tstart, 1, 10))
   tend = as.POSIXct(substr(tend, 1, 10))
   layerlist <- variable@z[[1]][variable@z[[1]] >= tstart & variable@z[[1]] <= tend]
-  
+
   if(fn == "mean"){
     agg_per_areas <- as.data.frame(lapply(layerlist, function(x){
       m <- variable[[which(variable@z[[1]] == x)]]
@@ -47,9 +49,9 @@ spatAggregate <- function(variable, spatialentity, tstart, tend, fn = "mean"){
     }))
     names(agg_per_areas) <- paste("max",substr(layerlist, start = 1, stop = 10), sep="_")
   }
-  
+
   return(agg_per_areas)
   #optionally the geometries could be returned with the aggregated vaules added to the data frame. Curently not used, see below:
-  #spatialentity@data <- cbind(spatialentity@data,means_per_areas) 
+  #spatialentity@data <- cbind(spatialentity@data,means_per_areas)
   #return(spatialentity)
 }
